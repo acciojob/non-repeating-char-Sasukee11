782ccc6079441a714f35f576eefd43b300ca8f88
Sasuke <118628015+Sasukee11@users.noreply.github.com>
2023-07-13 12:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const a = prompt('Enter a string');
+const arr = [...a];
+arr.sort();
+for(let i =0 ;i<arr.length-1;i++){
+	if(arr[i] !== arr[i+1]){
+		alert(arr[i]);
+	}
+}
